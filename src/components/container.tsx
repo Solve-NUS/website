f8aa37594e0351a772536da06aafcb578d49d7f1
Alvis Ng <alvis.ng.business@gmail.com>
2025-01-02 13:08:56 +0100

    Fix container padding for 100% zoom

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <main className="flex justify-center min-h-screen pt-16 pb-16">
+    <main className="flex justify-center min-h-screen pl-32 pr-32 pt-16 pb-16">
       <div className="container bg-white rounded-lg shadow-lg p-16 mt-8 flex flex-col justify-content items-center text-center gap-4 border-4 border-black">
         {children}
       </div>
