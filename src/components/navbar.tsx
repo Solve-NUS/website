0fe0c6fa1472b33fb6192f24ba48fa940e4e21e7
Alvis Ng <alvis.ng.business@gmail.com>
2025-01-02 15:40:51 +0100

    Add logo to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link'; // If using Next.js; otherwise, use <a> tags.
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50 h-16">
       <div className="container mx-auto flex items-center justify-between p-4">
         {/* Logo */}
-        <Link href="/" className="text-xl font-bold text-gray-800">
-          Solve@NUS
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo.svg"
+            alt="Solve@NUS Logo"
+            width={32} // Set the width of the image
+            height={32} // Set the height of the image
+            className="mr-2" // Space between the logo and text
+          />
+          <span className="text-xl font-bold text-gray-800">Solve@NUS</span>
         </Link>
 
         {/* Navigation Links */}
