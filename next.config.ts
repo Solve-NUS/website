7a2715c2c243a59c0f806075ccc37fa153b94689
Valerie Tan <valerietanhx@gmail.com>
2025-05-30 16:10:13 +0800

    test github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
+  basePath: "/website",
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
-  distDir: 'docs',
+  distDir: "docs",
 };
 
 export default nextConfig;
